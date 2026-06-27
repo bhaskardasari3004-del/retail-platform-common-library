3d5e7ce2968558574ce57bb85d14577ab20fc4cd
Bhaskar Dasari <bhaskar.dasari3004@gmail.com>
2026-06-27 11:30:04 +0400

    common library changes added

---
+++ src/test/java/com/retailbanking/common/RetailPlatformCommonLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retailbanking.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailPlatformCommonLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
