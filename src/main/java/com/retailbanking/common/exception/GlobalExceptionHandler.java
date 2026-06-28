838e5cd88d79c2156ce9ec431204a0c0bdfa6f81
Bhaskar Dasari <bhaskar.dasari3004@gmail.com>
2026-06-28 11:54:45 +0400

    common exceptions and API responses added

--- src/main/java/com/retailbanking/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/retailbanking/common/exception/GlobalExceptionHandler.java
@@ -1,4 +1,23 @@
 package com.retailbanking.common.exception;
 
+import com.retailbanking.common.response.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
+@RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
+
+
+    @ExceptionHandler(BusinessException.class)
+    public ResponseEntity<ApiResponse<Void>> handleBusinessException(BusinessException exception) {
+
+        ApiResponse<Void> response = new ApiResponse<>();
+        response.setSuccess(false);
+        response.setStatus(400);
+        return null;
+    }
 }
--- src/main/java/com/retailbanking/common/response/ApiResponse.java
+++ src/main/java/com/retailbanking/common/response/ApiResponse.java
@@ -1,9 +1,19 @@
 package com.retailbanking.common.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.Instant;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ApiResponse<T> {
     private boolean success;
+    private int status;
     private String message;
     private T data;
     private Instant timestamp;
